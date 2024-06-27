921a93fe786cadeedec9c4b9a49af8559a88e977
mail.exoticaitsolutions@gmail.com <mail.exoticaitsolutions@gmail.com>
2024-06-27 13:31:05 +0530

    Worked on the Dated 27-June-2024

--- README.md
+++ README.md
@@ -1,2 +1,139 @@
-# ABCGovtWebscrapping
-ABCGovtWebscrapping
+# Project Name
+## ABCGovt Web scrapping
+* This project focuses on creating a comprehensive tool for scraping data from various social media platforms such as Instagram, Twitter, and Facebook using Python and Django. The tool will allow users to gather and analyze social media data efficiently.
+### Overview
+* The Social Media Scraping Tool is designed to extract data from multiple social media platforms. This document details the module specifically for Twitter scraping, outlining the available APIs and their functionalities.
+
+# Setup Instructions
+
+## Installation
+
+### Python Installation Process
+Before proceeding, ensure Python is installed on your system. If not, you can download and install Python from [python.org](https://www.python.org/downloads/).
+
+
+
+### Installing Django
+Once the virtual environment is set up, you can install Django within it. Refer to the [Django documentation](https://docs.djangoproject.com/en/stable/intro/install/) for detailed instructions on installing Django.
+
+## Getting Started
+
+### Clone the Project
+```bash
+git clone https://github.com/exoticaitsolutions/ABCGovtWebscrapping.git
+```
+
+## Navigate to the Project Directory
+
+```bash
+  cd ABCGovtWebscrapping
+```
+### Setting up a Virtual Environment
+To work with Django, it's recommended to create a virtual environment. Follow the steps outlined in the [Python documentation](https://docs.python.org/3/tutorial/venv.html) or use tools like `virtualenv` or `venv`.
+Windows:
+```
+py -m venv myworld
+```
+Unix/MacOS:
+```
+ python3 -m venv myworld
+```
+Then you have to activate the environment, by typing this command:
+Windows:
+```
+myworld\Scripts\activate.bat
+```
+Unix/MacOS:
+```
+source myworld/bin/activate
+```
+
+# Install Dependencies
+### Using requirements.txt
+```
+pip install -r requirements.txt
+```
+
+# Individual Dependencies
+
+***pyppeteer***
+```
+pip install pyppeteer
+```
+***Fake Useragent***
+```
+pip install fake-useragent
+```
+
+***Setuptools***
+```
+python -m pip install --upgrade pip setuptools
+```
+***Wbdriver Manager***
+```
+pip install webdriver-manager
+```
+***selenium-wire***
+```
+pip install selenium-wire
+```
+
+***python-dotenv***
+```
+pip install python-dotenv
+```
+
+## Environment Variables
+ To run this project, you will need to add the following environment variables to your .env file
+# Create .env file
+in linux
+```
+touch .env
+```
+in window 
+```
+type nul > .env
+```
+## Setup .env File 
+```
+PAIDPROXY = FALSE
+PROXY_HOST = PROXY_HOST
+PROXY_PORT = PROXY_PORT
+PROXY_USERNAME = PROXY_USERNAME
+PROXY_PASSWORD = PROXY_PASSWORD
+```
+## Chromedriver
+* Ensure that the version of Chromedriver matches the version of Google Chrome installed on your system for proper functionality.
+# Redish
+```
+sudo apt install redis-server
+```
+```commandline
+sudo apt install redis-tools
+```
+```commandline
+redis-cli ping
+```
+```commandline
+redis-cli
+```
+```commandline
+redis-server
+```
+```commandline
+pip install django-redis
+```
+# Run Project
+**Windows:**
+
+```bash
+python manage.py runserver
+```
+
+**Unix/MacOS/LLinux:**
+
+```bash
+python3 manage.py runserver
+```
+
+
---
+++ scrapping_twinkker.py
@@ -0,0 +1,221 @@
+"""
+scrapping_twinkker.py
+
+This script automates web scraping of licensing reports from ABC Liquor website
+using Pyppeteer for headless browser automation. It provides a GUI interface
+using Tkinter for user interaction to select date ranges and generate daily reports.
+
+Libraries:
+- asyncio: For asynchronous operations.
+- tkinter: GUI toolkit.
+- datetime: Date and time manipulation.
+- pyppeteer: Headless browser automation.
+- tkcalendar: DateEntry widget for date selection in Tkinter.
+
+Functions:
+- scrape_and_save_table_data: Asynchronously scrapes and saves table data from the website.
+- start_scraping_genrating_daily_report_async: Initiates scraping based on user-selected date range.
+- generating_report: Callback function for GUI button to start the scraping process.
+- Other utility functions imported from 'utils.py' for printing output and saving data to file.
+
+"""
+import asyncio
+import os
+import time
+import tkinter as tk
+from datetime import datetime, timedelta
+from tkinter import messagebox, filedialog
+from pyppeteer import launch
+from pyppeteer.errors import PageError, TimeoutError, ElementHandleError
+
+from tkcalendar import DateEntry
+
+from utils import print_the_output_statement, save_data_to_file
+
+# Application constants
+APP_TITLE = "Daily License Report"
+APP_HEADING = "Welcome to the ABC Liquor Daily Report App"
+APP_BUTTON_NAME = "Generate Report"
+PAGE_URL = "https://www.abc.ca.gov/licensing/licensing-reports/new-applications/"
+HEADLESS = True
+
+
+# Function to scrape and save table data from the specified date range
+async def scrape_and_save_table_data(start_date, end_date, output):
+    start_time = time.time()
+    print_the_output_statement(output, 'Processing... Please wait for the generating the report.')
+    executable_path = '/usr/bin/google-chrome'
+
+    # Launching the browser
+    browser = await launch({
+        'headless': HEADLESS,
+        'executablePath': executable_path,
+        'defaultViewport': None
+    })
+    page = await browser.newPage()
+
+    try:
+        print('Launching browser..')
+        await page.goto(PAGE_URL, waitUntil='domcontentloaded')
+        print(f"Opening page from URL: {PAGE_URL}")
+        combined_data = []
+        combined_headers = []
+
+        # Converting date strings to datetime objects
+        current_date = datetime.strptime(start_date, '%B %d, %Y')
+        end_date = datetime.strptime(end_date, '%B %d, %Y')
+
+        # Loop through each date in the range
+        while current_date <= end_date:
+            formatted_date = current_date.strftime('%B_%d_%Y')
+            await page.waitForSelector('#daily-report-datepicker')
+            await page.click('#daily-report-datepicker')
+
+            await page.waitForSelector('.ui-datepicker-calendar', timeout=60000)
+            viewport_height = await page.evaluate('window.innerHeight')
+            scroll_distance = int(viewport_height * 0.3)
+
+            await page.evaluate(f'window.scrollBy(0, {scroll_distance})')
+
+            await page.type('#daily-report-datepicker', current_date.strftime('%B %d, %Y'))
+            print_the_output_statement(output, f"Generating Report on the dated  {formatted_date}...")
+            print(f"Typed '{formatted_date}' into the input box")
+            await page.waitForSelector('#daily-report-submit')
+            await page.click('#daily-report-submit')
+
+            await asyncio.sleep(5)
+
+            scroll_distance = int(viewport_height * 1.1)
+            await page.evaluate(f'window.scrollBy(0, {scroll_distance})')
+
+            try:
+                await page.waitForSelector('select[name="license_report_length"]', timeout=60000)
+                await page.click('select[name="license_report_length"]')
+                await page.waitForSelector('select[name="license_report_length"] option', timeout=60000)
+                await page.select('select[name="license_report_length"]', '100')
+                await asyncio.sleep(5)
+            except ElementHandleError as e:
+                print_the_output_statement(output, f"No data found for {formatted_date}: {str(e)}")
+                current_date += timedelta(days=1)
+                continue
+
+            try:
+                # Extracting table headers
+                headers = await page.evaluate('''() => {
+                    const table = document.querySelector('.display.table.table-striped.dataTable.no-footer');
+                    const headerRow = table.querySelector('thead tr');
+                    return Array.from(headerRow.querySelectorAll('th')).map(header => header.innerText.trim());
+                }''')
+
+                if not combined_headers:
+                    combined_headers = headers + ["Report Date"]
+
+                while True:
+                    # Extracting table data
+                    table_data = await page.evaluate('''() => {
+                        const table = document.querySelector('.display.table.table-striped.dataTable.no-footer');
+                        const rows = Array.from(table.querySelectorAll('tbody tr'));
+                        return rows.map(row => {
+                            const columns = Array.from(row.querySelectorAll('td, th'));
+                            return columns.map(column => column.innerText.trim());
+                        });
+                    }''')
+
+                    for row in table_data:
+                        row.append(current_date.strftime('%B %d, %Y'))
+                        combined_data.append(row)
+
+                    next_button = await page.querySelector('#license_report_next')
+                    if not next_button:
+                        print_the_output_statement(output, f"No more data found for {formatted_date}")
+                        break  # Exit loop if "Next" button not found
+
+                    is_disabled = await page.evaluate('(nextButton) => nextButton.classList.contains("disabled")',
+                                                      next_button)
+                    if is_disabled:
+                        break
+
+                    await next_button.click()
+                    print(f"Clicked on Next button for next page")
+                    await page.waitForSelector('#license_report tbody tr', timeout=30000)
+                    await asyncio.sleep(5)  # Adjust as needed
+                print_the_output_statement(output, f"Data found for {formatted_date}.")
+            except (PageError, ElementHandleError) as e:
+                print_the_output_statement(output, f"No data found for {formatted_date}: {str(e)}")
+            current_date += timedelta(days=1)
+
+        # Asking user to select a folder to save the data
+        save_folder = filedialog.askdirectory(initialdir=os.getcwd(), title="Select Folder to Save Data")
+        if save_folder:
+            file_name = save_data_to_file(combined_headers, combined_data, save_folder)
+            print_the_output_statement(output,
+                                       f"Download the Generated Report on the {save_folder} file is {file_name}")
+
+    finally:
+        await browser.close()
+        end_time = time.time()
+        total_time = end_time - start_time
+        print_the_output_statement(output, f"Total execution time: {total_time:.2f} seconds")
+# Function to validate date inputs and start the scraping process
+async def start_scraping_genrating_daily_report_async(
+        start_date_str, end_date_str, outputted
+):
+    current_date_str = datetime.now().date().strftime("%B %d, %Y")
+    current_date = datetime.strptime(current_date_str, "%B %d, %Y").date()
+    print("current_date", current_date)
+    start_date = datetime.strptime(start_date_str, "%B %d, %Y").date()
+    print("start_date", start_date)
+    end_date = datetime.strptime(end_date_str, "%B %d, %Y").date()
+    print("end_date", end_date)
+    if start_date > current_date or end_date > current_date:
+        messagebox.showerror(
+            "Error", "Please select a date that is 2 or more days past."
+        )
+    elif end_date < start_date:
+        messagebox.showerror(
+            "Error", "End date should be later than or equal to start date."
+        )
+    else:
+        await scrape_and_save_table_data(start_date_str, end_date_str, outputted)
+
+
+# Function to handle the button click event
+def generating_report():
+    start_date_str = start_date_entry.get_date().strftime("%B %d, %Y")
+    end_date_str = end_date_entry.get_date().strftime("%B %d, %Y")
+    asyncio.run(
+        start_scraping_genrating_daily_report_async(
+            start_date_str, end_date_str, output_text
+        )
+    )
+
+
+# Setting up the Tkinter GUI
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title(APP_TITLE)
+    heading_label = tk.Label(
+        root, text=APP_HEADING, font=("Helvetica", 18, "bold"), pady=20
+    )
+    heading_label.pack()
+    form_frame = tk.Frame(root)
+    form_frame.pack(pady=20)
+    start_date_label = tk.Label(form_frame, text="Start Date:")
+    start_date_label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+    start_date_entry = DateEntry(
+        form_frame, width=12, background="darkblue", foreground="white", borderwidth=2
+    )
+    start_date_entry.grid(row=0, column=1, padx=10, pady=10)
+    end_date_label = tk.Label(form_frame, text="End Date:")
+    end_date_label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+    end_date_entry = DateEntry(
+        form_frame, width=12, background="darkblue", foreground="white", borderwidth=2
+    )
+    end_date_entry.grid(row=1, column=1, padx=10, pady=10)
+    scrape_button = tk.Button(root, text=APP_BUTTON_NAME, command=generating_report)
+    scrape_button.pack()
+    output_text = tk.Text(root, height=10, width=100)
+    output_text.tag_configure("bold", font=("Arial", 12, "bold"))
+    output_text.pack(pady=20)
+    root.mainloop()
+    print("Script execution completed!")
